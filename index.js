3355ea6f2e003e6ef7fab0357107830cb381ec7b
jaggu24 <hanumanjeyakumaran@gmail.com>
2020-07-31 20:50:17 +0530

    added auth for signIn/Up

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require("express");
 const app = express();
 const auth = require("./routes/auth");
 
-app.use(auth);
+app.use("/auth", auth);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/post", (req, res) => {
-  res.send({ posts: "postid" });
-});
-
-app.post("/addPost", (req, res) => {
-  res.send({ sendId: req.body.postId });
-});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listerning to ${port}....`));

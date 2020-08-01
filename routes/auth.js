e631db3f78ffe894d51984e3d2204e0d5c978ab1
jaggu24 <hanumanjeyakumaran@gmail.com>
2020-08-01 19:03:42 +0530

    auth.js: solved invalid user creditional

--- routes/auth.js
+++ routes/auth.js
@@ -59,22 +59,25 @@ auth.get("/signIn/:username/:password", (req, res) => {
     password: req.params.password
   });
   let data = [];
-  if (typeof result.error === "undefined") {
-    db.query(
-      "select password from `auth` where username=?",
-      [result.value.username],
-      function(error, results, fields) {
-        if (error)
-          return res.status(404).send({ status: 404, error: error.message });
-        if (decrypt(results[0].password) === result.value.password)
-          return res.send({ status: 200, data: "Login Sucessful" });
-        else
-          return res.send({ status: 200, data: "Invalid User creditionals" });
-      }
-    );
-  } else {
-    return res.status(400).send({ status: 400, error: result.error.message });
+  try {
+    if (typeof result.error === "undefined") {
+      db.query(
+        "select password from `auth` where username=?",
+        [result.value.username],
+        function(error, results, fields) {
+          if (error)
+            return res.status(404).send({ status: 404, error: error.message });
+          if (decrypt(results[0].password) === result.value.password)
+            return res.send({ status: 200, data: "Login Sucessful" });
+          else
+            return res.send({ status: 200, data: "Invalid User creditionals" });
+        }
+      );
+    } else {
+      return res.status(400).send({ status: 400, error: result.error.message });
+    }
+  } catch (err) {
+    return res.send({ status: 400, data: "Invalid User creditionals" });
   }
-  return console.log(1);
 });
 module.exports = auth;
